487dc3bffe0bc1ee3e54caa116524afed16db8e7
nish1013 <nishand2d@gmail.com>
2023-07-17 22:50:19 +0100

    updated css

--- src/components/donation-panel/DonationPanel.js
+++ src/components/donation-panel/DonationPanel.js
@@ -5,21 +5,23 @@ import PaymentOptions from "../payment-options/PaymentOptions";
 export default function DonationPanel() {
   //TODO organise tailwindcss
   const classRegular =
-    "Button w-1/2 border hover:text-white font-bold my-2 mx-2 py-2 px-4 rounded-full";
+    "Button w-1/4 border hover:text-white font-bold my-2 mx-2 py-2 px-4 rounded-full";
   const classValueBtns =
-    "Button w-50 border hover:text-white font-bold mx-2 py-2 px-4 rounded-full";
+    "Button w-1/8 border hover:text-white font-bold mx-2 py-2 px-4 rounded-full";
   const classDonateBtn =
-    "DonateButton w-full bg-amber-400 hover:bg-amber-500 font-bold my-4 py-4 px-4 rounded-full";
+    "DonateButton w-1/2 self-center bg-amber-400 hover:bg-amber-500 font-bold my-4 py-4 px-4 rounded-full";
   return (
     <div className="DonationPanel flex flex-col justify-center justify-items-center py-60 px-8 bg-amber-500 sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2 lg:min-h-screen xl:min-h-screen 2xl:min-h-screen">
+     <div className="flex flex-col items-center">
       <h1 className="text-4xl font-bold">Make a</h1>
       <h1 className="text-4xl font-bold">donation</h1>
       <p className="py-2">Do something amazing</p>
-      <div className="flex justify-even">
+      </div>
+      <div className="flex justify-center">
         <button className={classRegular}>Single</button>
         <button className={classRegular}>Regular</button>
       </div>
-      <div>
+      <div className="flex justify-center">
         <button className={classValueBtns}>£5</button>
         <button className={classValueBtns}>£10</button>
         <button className={classValueBtns}>£20</button>
