0026111b1f41db5c6b5739576f5baa1f3a810c8d
nish1013 <nishand2d@gmail.com>
2023-07-18 13:25:41 +0100

    updated button css

--- src/components/donation-panel/Amount.js
+++ src/components/donation-panel/Amount.js
@@ -3,9 +3,9 @@ import * as React from "react"
 export default function Amount({amount=0, selected=false, onClickBtn}) {
     
     const classValueBtn =
-    "Button min-w-1/8 border hover:text-white font-bold mx-2 py-2 px-4 rounded-full";
+    "Button w-1/7 min-w-1/8 border hover:text-white font-bold mx-1 py-3 px-4 rounded-full";
     const classValueSelectedBtn =
-    "Button-selected min-w-1/8 border text-white font-bold mx-2 py-2 px-4 rounded-full";
+    "Button-selected w-1/7 min-w-1/8 border text-white font-bold mx-2 py-3 px-4 rounded-full";
     const className = selected ? classValueSelectedBtn: classValueBtn;
     return (<div onClick={onClickBtn} className={className}>£{amount}</div>)
 }
