f806dcb83df412d0f8c555cf11574d062ca53297
nish1013 <nishand2d@gmail.com>
2023-07-18 09:13:41 +0100

    removing breakpoints for now

--- src/components/image-panel/ImagePanel.js
+++ src/components/image-panel/ImagePanel.js
@@ -2,9 +2,9 @@ import * as React from "react"
 import image from "../../images/donation-photo.jpg"
 export default function ImagePanel() {
   return (
-    <div className="sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/2 min-h-screen">
+    <div className="w-full">
       <img
-        className="object-contain md:object-scale-down"
+        className=""
         alt="donations"
         src={image}
       />
